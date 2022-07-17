d464b9a3bb37887ba3c612a65980832af9ce428e
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-07-18 00:48:13 +0100

    added footer and fixed styling issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 console.log("working js")
-const button = document.querySelector('[data-btn]')
+const addButton = document.querySelector('[data-btn]')
 const clearButton = document.querySelector('[data-clear-btn]')
 
 let ul = document.querySelector('[data-ul]')
@@ -10,23 +10,25 @@ let ul = document.querySelector('[data-ul]')
 for (let index = 0; index < localStorage.length; index++) {
     let li = document.createElement('li')
     li.textContent = localStorage.getItem(`item ${index}`)
+    li.classList.add('li')
     ul.appendChild(li)
-    
 }
 
-button.addEventListener('click',()=>{
+addButton.addEventListener('click',()=>{
     let ul = document.querySelector('[data-ul]')
     const input = document.querySelector('[data-input]')
+    let inputValue = String(input.value)
+    input.value = ''
     let item = ''
     let listLength = ul.children.length
     if(listLength){
         console.log('ul not empty')
         console.log(listLength)
-        localStorage.setItem(`item ${listLength}`, String(input.value))
+        localStorage.setItem(`item ${listLength}`, inputValue)
         item = `item ${listLength}`
     }else{
         console.log('ul empty')
-        localStorage.setItem(`item 0`, String(input.value))
+        localStorage.setItem(`item 0`, inputValue)
         item = `item 0`
     }
     let li = document.createElement('li')
